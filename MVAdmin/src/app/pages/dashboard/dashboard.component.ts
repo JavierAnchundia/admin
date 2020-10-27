f26fe50c9e624e68680025cd51b3e7efc0878c88
bryantutiven2 <bryan.tutiven.2@outlook.com>
2020-10-27 15:31:12 -0500

    Se reestructuro el html, css y ts para implementar el diseño propuesto por la diseñadora

--- MVAdmin/src/app/pages/dashboard/dashboard.component.ts
+++ MVAdmin/src/app/pages/dashboard/dashboard.component.ts
@@ -6,7 +6,9 @@ import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { UsuarioService } from 'src/app/services/usuario/usuario.service';
 import URL_SERVICIOS from 'src/app/config/config';
-
+import Swal from 'sweetalert2'
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { faUserTag } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -15,15 +17,71 @@ import URL_SERVICIOS from 'src/app/config/config';
 export class DashboardComponent implements OnInit {
   lista_camposanto: any[] = [];
   url_backend: String = URL_SERVICIOS.url_backend;
+  nameUsuario: string = 'Administrador';
+  tipoUsuario: string = 'Administrador'
+  id: any;
+  loggeduser = false;
+  faTimesCircle = faTimesCircle;
+  faUserTag = faUserTag;
+
   constructor(
     public _servicio: CamposantoService, 
     private router: Router,
-    private _usuarioService : UsuarioService) { 
+    private _usuarioService : UsuarioService,
+    ) { 
   }
 
   ngOnInit(): void {
     this._usuarioService.loadStorage();
     this.cargarCamposantos();
+    this.id = JSON.stringify(localStorage.getItem('id'));
+    //this.getUser();
+    let tipo = localStorage.getItem('tipo_user');
+    this.verificarTipoUser(tipo);
+  }
+
+  verificarTipoUser(tipo){
+    if(tipo == 'ha'){
+      this.tipoUsuario = 'Hyper Administrador';
+    }
+    else if(tipo == 'su'){
+      this.tipoUsuario = 'Super Administrador'
+    }
+    else if(tipo == 'ad'){
+      this.tipoUsuario = 'Administrador'
+    }
+    // this.getStatus();
+    this.nameUsuario = localStorage.getItem('username');
+  }
+
+  // getStatus() {
+  //   this.loggeduser = this._usuarioService.isLoggedin;
+  //   return this.loggeduser;
+  // }
+
+
+  logout() {
+    Swal.fire({
+      title: '¿Está seguro que desea salir?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí',
+      cancelButtonText: 'No'
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          '¡Se ha cerrado sesión!',
+          'Sesión cerrada exitosamente'
+        )
+        this._usuarioService.logoutUser();
+        this.router.navigate(['/login'])
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelado',
+          'El cierre de sesión se ha cancelado'
+        )
+      }
+    })
   }
 
   cargarCamposantos() {
